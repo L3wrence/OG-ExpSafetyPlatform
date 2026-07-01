271dbffd51dc7f0ed9355e0cfea8997d29820181
L3wrence <L3wrence@users.noreply.github.com>
2026-07-01 19:30:05 +0800

    初始化前后端项目结构

---
+++ backend/OG-ExpSafetyPlatform/src/main/java/com/cupk/OgExpSafetyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.cupk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgExpSafetyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgExpSafetyPlatformApplication.class, args);
+    }
+
+}
